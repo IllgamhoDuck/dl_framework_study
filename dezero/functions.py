17e6728ae0b5ff3ef580f319d00497fb0e76633d
illgamhoDuck <hyunbyung87@gmail.com>
2020-12-27 01:56:04 +0900

    Neural Network implementation and refactor functions.py

--- dezero/functions.py
+++ dezero/functions.py
@@ -5,6 +5,28 @@ from dezero import Function
 from dezero.core import as_variable
 
 
+# Neural Network
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+# Calculation
+
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
@@ -35,6 +57,44 @@ class Tanh(Function):
         gx = gy * (1 - y**2)
         return gx
 
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y
+        return gx
+
+class Log(Function):
+    def forward(self, x):
+        y = np.log(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy / x
+        return gx
+
+def sin(x):
+    return sin()(x)
+
+def cos(x):
+    return cos()(x)
+
+def tanh(x):
+    return tanh()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def log(x):
+    return Log()(x)
+
+
+# Shape transform
+
 class Reshape(Function):
     def __init__(self, shape):
         self.shape = shape
@@ -63,6 +123,17 @@ class Transpose(Function):
             inv_axes = tuple(np.argsort([ax % axes_len for ax in self.axes]))
             gx = transpose(gy, inv_axes)
         return gx
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
+def transpose(x, axes=None):
+    return Transpose(axes)(x)
+
+
+# Broadcast
 
 class Sum(Function):
     def __init__(self, axis=None, keepdims=False):
@@ -105,6 +176,22 @@ class SumTo(Function):
         gx = broadcast_to(gy, self.x_shape)
         return gx
 
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
+
+def broadcast_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return BroadcastTo(shape)(x)
+
+def sum_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
+
+
+# Matrix
+
 class MatMul(Function):
     def forward(self, x, W):
         y = x.dot(W)
@@ -115,6 +202,29 @@ class MatMul(Function):
         gx = matmul(gy, W.T)
         gW = matmul(x.T, gy)
         return gx, gW
+
+def matmul(x, W):
+    return MatMul()(x, W)
+
+
+# activation
+
+class Sigmoid(Function):
+    def forward(self, x):
+        # y = 1 / (1 + np.exp(-x))
+        y = np.tanh(x * 0.5) * 0.5 + 0.5
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+
+# Loss
 
 class MeanSquaredError(Function):
     def forward(self, x0, x1):
@@ -129,39 +239,22 @@ class MeanSquaredError(Function):
         gx1 = -gx0
         return gx0, gx1
 
-
-def sin(x):
-    return sin()(x)
-
-def cos(x):
-    return cos()(x)
-
-def tanh(x):
-    return tanh()(x)
-
-def reshape(x, shape):
-    if x.shape == shape:
-        return as_variable(x)
-    return Reshape(shape)(x)
-
-def transpose(x, axes=None):
-    return Transpose(axes)(x)
-
-def sum(x, axis=None, keepdims=False):
-    return Sum(axis, keepdims)(x)
-
-def broadcast_to(x, shape):
-    if x.shape == shape:
-        return as_variable(x)
-    return BroadcastTo(shape)(x)
-
-def sum_to(x, shape):
-    if x.shape == shape:
-        return as_variable(x)
-    return SumTo(shape)(x)
-
-def matmul(x, W):
-    return MatMul()(x, W)
-
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
+
+
+
+# simplified versions
+
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b in None:
+        return t
+
+    y = t + b
+
+    # t is only used for back propagation
+    # The value inside doesn't matter so we can remove it
+    # to save memory
+    t.data = None
+    return y
