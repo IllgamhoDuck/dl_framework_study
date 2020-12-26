f52df85d16b1ceca7dc8b5f68425b1d48ee2b97c
illgamhoDuck <hyunbyung87@gmail.com>
2020-12-26 22:31:33 +0900

    linear regression test

--- dezero/functions.py
+++ dezero/functions.py
@@ -116,6 +116,19 @@ class MatMul(Function):
         gW = matmul(x.T, gy)
         return gx, gW
 
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / len(diff)
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
 
 def sin(x):
     return sin()(x)
@@ -149,3 +162,6 @@ def sum_to(x, shape):
 
 def matmul(x, W):
     return MatMul()(x, W)
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
