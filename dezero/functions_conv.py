90caeccacce5892d6489bd9d17d80452e600cf01
illgamhoDuck <hyunbyung87@gmail.com>
2021-01-09 21:00:26 +0900

    col2im implemented for cpu & gpu. notebook instruction complete for both im2col and col2im numpy version

--- dezero/functions_conv.py
+++ dezero/functions_conv.py
@@ -4,6 +4,8 @@ from dezero import cuda
 import numpy as np
 
 
+### Numpy im2col & col2im
+
 def im2col_array(img, kernel_size, stride, pad, to_matrix=True):
     N, C, H, W = img.shape
     KH, KW = pair(kernel_size)
@@ -34,6 +36,34 @@ def im2col_array(img, kernel_size, stride, pad, to_matrix=True):
         col = col.transpose((0, 4, 5, 1, 2, 3)).reshape(N * OH * OW, -1)
 
     return col
+
+def col2im_array(col, img_shape, kernel_size, stride, pad, to_matrix=True):
+    N, C, H, W = img_shape
+    KH, KW = pair(kernel_size)
+    SH, SW = pair(stride)
+    PH, PW = pair(pad)
+
+    OH = get_conv_outsize(H, KH, SH, PH)
+    OW = get_conv_outsize(W, KW, SW, PW)
+
+    if to_matrix:
+        col = col.reshape(N, OH, OW, C, KH, KW).transpose(0, 3, 4, 5, 1, 2)
+
+    xp = cuda.get_array_module(col)
+    if xp != np:
+        img = _col2im_gpu(col, SH, SW, PH, PW, H, W)
+    else:
+        img = np.zeros((N, C, H + 2 * PH + SH - 1, W + 2 * PW + SW - 1),
+                      dtype=col.dtype)
+
+        for j in range(KH):
+            j_lim = j + SH * OH
+            for i in range(KW):
+                i_lim = i + SH * OH
+
+                img[:, :, j:j_lim:SH, i:i_lim:SW] += col[:, :, j, i, :, :]
+
+        return img[:, :, PH:H + PH, PW:W + PW]
 
 def _im2col_gpu(img, kernel_size, stride, pad):
     """img2col function for GPU
@@ -75,3 +105,48 @@ def _im2col_gpu(img, kernel_size, stride, pad):
                  h, w, out_h, out_w, kh, kw, sy, sx, ph, pw, dy, dx, col)
 
     return col
+
+def _col2im_gpu(col, sy, sx, ph, pw, h, w):
+    """col2im function for GPU.
+    This code is ported from Chainer:
+    https://github.com/chainer/chainer/blob/v6.4.0/chainer/utils/conv.py
+    """
+    n, c, kh, kw, out_h, out_w = col.shape
+    dx, dy = 1, 1
+    img = cuda.cupy.empty((n, c, h, w), dtype=col.dtype)
+
+    cuda.cupy.ElementwiseKernel(
+        'raw T col, int32 h, int32 w, int32 out_h, int32 out_w, '
+        'int32 kh, int32 kw, int32 sy, int32 sx, int32 ph, int32 pw,'
+        'int32 dx, int32 dy',
+        'T img',
+        '''
+            int c0 = i / (h * w);
+            int y = i / w % h;
+            int x = i % w;
+
+            T val = 0;
+
+            for (int ky = 0; ky < kh; ++ky){
+                int out_y = (y + ph - ky * dy);
+                if (0 > out_y || out_y >= out_h * sy) continue;
+                if (out_y % sy != 0) continue;
+                out_y /= sy;
+
+                for (int kx = 0; kx < kw; ++kx){
+                    int out_x = (x + pw - kx * dx)
+                    if (0 > out_x || out_x >= out_w * sx) continue;
+                    if (out_x % sx != 0) continue;
+                    out_x /= sx;
+
+                    int k = out_y + out_h * (kx + kw * (ky + kh * c0));
+                    val = val + col[out_x + out_w * k];
+                }
+            }
+            img = val;
+
+        ''',
+        'col2im')(col.reduced_view(),
+                  h, w, out_h, out_w, kh, kw, sy, sx, ph, pw, dx, dy, img)
+
+    return img
